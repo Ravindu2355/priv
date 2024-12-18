5661d8caeb65cfd089fb79047bfb5d7d21a427cb
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-12-18 10:02:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def login_user_client(_,phone_number: str, message: Message):
         await message.reply("alredy loged")
         return
     user_id = message.chat.id
-    number = await ask(_,user_id, 'Please enter your phone number along with the country code. \nExample: +19876543210', filters=filters.text)   
+    number = await ask(_,user_id, 'Please enter your phone number along with the country code. \nExample: +19876543210')   
     phone_number = number.text
     try:
         await message.reply("📲 Sending OTP...")
@@ -84,7 +84,7 @@ async def login_user_client(_,phone_number: str, message: Message):
         await message.reply('❌ Invalid phone number. Please restart the session.')
         return
     try:
-        otp_code = await ask(_,user_id, "Please check for an OTP in your official Telegram account. Once received, enter the OTP in the following format: \nIf the OTP is `12345`, please enter it as `1 2 3 4 5`.", filters=filters.text, timeout=600)
+        otp_code = await ask(_,user_id, "Please check for an OTP in your official Telegram account. Once received, enter the OTP in the following format: \nIf the OTP is `12345`, please enter it as `1 2 3 4 5`.", timeout=600)
     except TimeoutError:
         await message.reply('⏰ Time limit of 10 minutes exceeded. Please restart the session.')
         return
@@ -100,7 +100,7 @@ async def login_user_client(_,phone_number: str, message: Message):
         return
     except SessionPasswordNeeded:
         try:
-            two_step_msg = await ask(_,user_id, 'Your account has two-step verification enabled. Please enter your password.', filters=filters.text, timeout=300)
+            two_step_msg = await ask(_,user_id, 'Your account has two-step verification enabled. Please enter your password.', timeout=300)
         except TimeoutError:
             await message.reply('⏰ Time limit of 5 minutes exceeded. Please restart the session.')
             return
